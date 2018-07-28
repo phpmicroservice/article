b8d8db13017afeeff8f4a40caf5dcf9bc4a26714
Dongasai <1514582970@qq.com>
2018-07-28 15:30:53 +0800

    升级依赖  修改接口  Changes to be committed: 	modified:   model/article.php
     Changes not staged for commit:
    	modified:   ../composer.lock

--- app/model/article.php
+++ app/model/article.php
@@ -9,6 +9,11 @@ namespace app\model;
  */
 class article extends \pms\Mvc\Model
 {
-
+    protected $_append_field = ['content_source'];
+    public function afterFetch()
+    {
+       #content
+        $this->content_source=base64_decode($this->content);
+    }
 
 }
